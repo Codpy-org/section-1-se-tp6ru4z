f0a6f9597dcec527bcf11e472900310146cb1d40
tp6ru4z <tp6ru4z@gmail.com>
2025-03-16 17:10:09 +0800

    fix: finish unit test

--- UnitTestHW/Fizz Buzz/Basic/horrible_fizzbuzz.py
+++ UnitTestHW/Fizz Buzz/Basic/horrible_fizzbuzz.py
@@ -1,9 +1,10 @@
 def fizzBuzz(n: int) -> str:
+    if n%3==0:
+        return "Fizz"
+    if n%5==0:
+        return "Buzz"
     if n%15==0:
         return "Fizz" + "Buzz"
-    elif n%3==0:
-        return "Fizz"
-    elif n%5==0:
-        return "Buzz"
-    else :
+    elif n%1==0:
         return f"{str(n)}"
+    return "OwO"
